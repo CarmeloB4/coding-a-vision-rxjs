2ad213defe678d1fff1ad7fc861cddc40b5295cd
Piero Cascio <piero.cascio@gmail.com>
2020-09-05 17:01:38 +0200

    Add mocked data.

---
+++ scripts/mock-data/data.js
@@ -0,0 +1,13 @@
+export const beers = [
+	{name: 'Stella', country: 'Belgium', price: 9.50},
+	{name: 'Sam Adams', country: 'USA', price: 8.50},
+	{name: 'Bud Light', country: 'USA', price: 6.50},
+	{name: 'Brooklyn Lager', country: 'USA', price: 8.00},
+	{name: 'Sapporo', country: 'Japan', price: 7.50},
+]
+
+export const softDrinks = [
+	{name: 'Coca Cola', country: 'USA', price: 1.50},
+	{name: 'Fanta', country: 'USA', price: 1.50},
+	{name: 'Lemonade', country: 'France', price: 2.50},
+]
